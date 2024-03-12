6db0d94033f3a86a03738b03ab4ded4ce69ad2a2
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2024-03-12 21:50:25 +0800

    create login page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import TemperatureGraph from './pages/TemperatureGraph';
 import HumidityGraph from './pages/HumidityGraph';
+import LoginPage from './pages/LoginPage';
 
 const queryClient = new QueryClient();
 
@@ -11,11 +12,12 @@ const App = () => {
 	return (
 		<QueryClientProvider client={queryClient}>
 			<div className='container'>
-				<Navbar />
+				{/* <Navbar /> */}
 				<Routes>
 					<Route path='/' element={<Home />} />
 					<Route path='/temperature' element={<TemperatureGraph />} />
 					<Route path='/humidity' element={<HumidityGraph />} />
+					<Route path='login' element={<LoginPage />} />
 				</Routes>
 			</div>
 		</QueryClientProvider>
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+const LoginPage = () => {
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
+
+	const handleLogin = (event) => {
+		event.preventDefault();
+		// Handle login logic here
+		console.log(`Username: ${username}, Password: ${password}`);
+	};
+
+	return (
+		<Box
+			display='flex'
+			justifyContent='center'
+			alignItems='center'
+			minHeight='100vh'
+			bgcolor='#e9ecef'
+		>
+			<Container
+				maxWidth='xs'
+				sx={{
+					padding: 3,
+					borderRadius: 4,
+					backgroundColor: '#fff'
+				}}
+			>
+				<Typography variant='h4' align='center'>
+					Sign in
+				</Typography>
+				<form onSubmit={handleLogin}>
+					<TextField
+						variant='outlined'
+						margin='normal'
+						required
+						fullWidth
+						id='username'
+						label='Username'
+						name='username'
+						autoComplete='username'
+						autoFocus
+						value={username}
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+					<TextField
+						variant='outlined'
+						margin='normal'
+						required
+						fullWidth
+						name='password'
+						label='Password'
+						type='password'
+						id='password'
+						autoComplete='current-password'
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+					<Button
+						type='submit'
+						fullWidth
+						variant='contained'
+						color='primary'
+						style={{ marginTop: '1rem' }}
+					>
+						Login
+					</Button>
+				</form>
+			</Container>
+		</Box>
+	);
+};
+
+export default LoginPage;
