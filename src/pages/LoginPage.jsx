ebf104d6bca30c1e0e44a699e86ffbb7c66bcfb3
Abdul Pangandaman <abdulfahadgo566@gmail.com>
2024-04-15 20:07:16 +0800

    upgrade Login Page - added header and footer

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,6 +2,8 @@ import React, { useContext, useEffect, useState } from 'react';
 import { Button, TextField, Container, Typography, Box } from '@mui/material';
 import { AuthContext } from '../App';
 import { useNavigate } from 'react-router-dom';
+import LoginHeader from '../components/LoginHeader';
+import LoginFooter from '../components/LoginFooter';
 
 const LoginPage = () => {
 	const navigate = useNavigate();
@@ -33,75 +35,83 @@ const LoginPage = () => {
 	}, []);
 
 	return (
-		<Box
-			display='flex'
-			justifyContent='center'
-			alignItems='center'
-			minHeight='100vh'
-			bgcolor='#e9ecef'
-		>
-			<Container
-				maxWidth='xs'
-				sx={{
-					padding: 3,
-					borderRadius: 4,
-					backgroundColor: '#fff'
-				}}
+		<>
+			<LoginHeader />
+			<Box
+				display='flex'
+				justifyContent='center'
+				alignItems='center'
+				minHeight='calc(100vh - 209px)'
+				bgcolor='#e9ecef'
 			>
-				<Typography variant='h4' align='center'>
-					Sign in
-				</Typography>
-				{loginFailed && (
-					<Typography
-						variant='h6'
-						align='center'
-						style={{ color: 'red' }}
-						sx={{ fontSize: '1rem' }}
-					>
-						Invalid username or password
+				<Container
+					maxWidth='xs'
+					sx={{
+						padding: 3,
+						borderRadius: 4,
+						backgroundColor: '#fff'
+					}}
+				>
+					<Typography variant='body1' align='center'>
+						<span style={{ fontSize: '30px', fontWeight: 520 }}>
+							Sign in
+						</span>
 					</Typography>
-				)}
-				<form onSubmit={handleLogin}>
-					<TextField
-						variant='outlined'
-						margin='normal'
-						required
-						fullWidth
-						id='username'
-						label='Username'
-						name='username'
-						autoComplete='username'
-						autoFocus
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
-						error={loginFailed}
-					/>
-					<TextField
-						variant='outlined'
-						margin='normal'
-						required
-						fullWidth
-						name='password'
-						label='Password'
-						type='password'
-						id='password'
-						autoComplete='current-password'
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						error={loginFailed}
-					/>
-					<Button
-						type='submit'
-						fullWidth
-						variant='contained'
-						color='primary'
-						style={{ marginTop: '1rem' }}
-					>
-						Login
-					</Button>
-				</form>
-			</Container>
-		</Box>
+					{loginFailed && (
+						<Typography
+							variant='h6'
+							align='center'
+							style={{ color: 'red' }}
+							sx={{ fontSize: '1rem' }}
+						>
+							Invalid username or password
+						</Typography>
+					)}
+					<form onSubmit={handleLogin}>
+						<TextField
+							variant='outlined'
+							margin='normal'
+							required
+							fullWidth
+							id='username'
+							label='Username'
+							name='username'
+							autoComplete='username'
+							autoFocus
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+							error={loginFailed}
+						/>
+						<TextField
+							variant='outlined'
+							margin='normal'
+							required
+							fullWidth
+							name='password'
+							label='Password'
+							type='password'
+							id='password'
+							autoComplete='current-password'
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							error={loginFailed}
+						/>
+						<Button
+							type='submit'
+							fullWidth
+							variant='contained'
+							sx={{
+								backgroundColor: '#a64d4d',
+								marginTop: '1rem'
+							}}
+						>
+							Login
+						</Button>
+					</form>
+				</Container>
+			</Box>
+			<LoginFooter />
+		</>
 	);
 };
 
